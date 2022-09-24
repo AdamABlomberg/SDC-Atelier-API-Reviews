bca2cbeaab260d213f85a21500939475ba98f879
Adam Blomberg <adamablomberg@gmail.com>
2022-09-24 00:13:57 -0500

    update to test for nginix

--- tests/k6test.js
+++ tests/k6test.js
@@ -4,11 +4,11 @@ import { check, sleep } from 'k6';
 export const options = {
   stages: [
     { duration: '10s', target: 50 },
-    //   { duration: '10s', target: 100 },
-    //   { duration: '10s', target: 250 },
-    //   { duration: '10s', target: 500 },
-    //   { duration: '10s', target: 750 },
-    //   { duration: '10s', target: 1000 },
+      { duration: '10s', target: 100 },
+      { duration: '10s', target: 250 },
+      { duration: '10s', target: 500 },
+      { duration: '10s', target: 750 },
+      { duration: '10s', target: 1000 },
   ]
 }
 
